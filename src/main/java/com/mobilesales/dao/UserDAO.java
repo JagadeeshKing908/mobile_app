5480489e52d76b309afb95b842b06602ce70ba06
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-05 22:23:09 +0530

    Update UserDAO.java

--- src/main/java/com/mobilesales/dao/UserDAO.java
+++ src/main/java/com/mobilesales/dao/UserDAO.java
@@ -3,9 +3,11 @@ package com.mobilesales.dao;
 import com.mobilesales.config.DBConfig;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UserDAO {
 
+    // REGISTER USER
     public boolean registerUser(String username, String password) {
 
         String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
@@ -15,7 +17,6 @@ public class UserDAO {
 
             ps.setString(1, username);
             ps.setString(2, password);
-
             ps.executeUpdate();
             return true;
 
@@ -23,5 +24,28 @@ public class UserDAO {
             e.printStackTrace();
             return false;
         }
+    }
+
+    // GET PASSWORD FOR LOGIN
+    public String getPasswordByUsername(String username) {
+
+        String sql = "SELECT password FROM users WHERE username = ?";
+        String password = null;
+
+        try (Connection con = DBConfig.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                password = rs.getString("password");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return password;
     }
 }
