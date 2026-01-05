398ba45fd91c142455de1442019aaacb92cf48c1
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-05 21:31:36 +0530

    Create RegisterServlet.java

---
+++ src/main/java/com/mobilesales/servlet/RegisterServlet.java
@@ -0,0 +1,22 @@
+package com.mobilestore.servlet;
+
+import com.mobilestore.dao.UserDAO;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class RegisterServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String user = req.getParameter("username");
+        String pass = req.getParameter("password");
+
+        UserDAO dao = new UserDAO();
+        dao.registerUser(user, pass);
+
+        resp.sendRedirect("login.jsp");
+    }
+}
