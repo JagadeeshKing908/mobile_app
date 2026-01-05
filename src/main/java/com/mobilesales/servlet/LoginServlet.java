24c2604d6808599a43929f065517d3484761425c
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-05 22:25:56 +0530

    Update LoginServlet.java

--- src/main/java/com/mobilesales/servlet/LoginServlet.java
+++ src/main/java/com/mobilesales/servlet/LoginServlet.java
@@ -21,4 +21,12 @@ public class LoginServlet extends HttpServlet {
         UserDAO dao = new UserDAO();
         String dbPassword = dao.getPasswordByUsername(username);
 
-        if (dbPassword != null && dbPassword
+        if (dbPassword != null && dbPassword.equals(password)) {
+            response.sendRedirect("home.html");
+        } else {
+            response.sendRedirect("index.html?error=invalid");
+        }
+
+    } // end of doPost
+
+} // end of LoginServlet class
