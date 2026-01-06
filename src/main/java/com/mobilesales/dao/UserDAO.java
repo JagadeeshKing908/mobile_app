4633f7f4cf73ecc63a6ead4aa86af0fed1a12a79
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-06 19:15:47 +0530

    Update UserDAO.java

--- src/main/java/com/mobilesales/dao/UserDAO.java
+++ src/main/java/com/mobilesales/dao/UserDAO.java
@@ -1,51 +1,21 @@
 package com.mobilesales.dao;
 
 import com.mobilesales.config.DBConfig;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class UserDAO {
 
-    // REGISTER USER
-    public boolean registerUser(String username, String password) {
+    public String login(String username, String password) throws Exception {
+        Connection con = DBConfig.getConnection();
+        PreparedStatement ps =
+            con.prepareStatement("SELECT role FROM users WHERE username=? AND password=?");
+        ps.setString(1, username);
+        ps.setString(2, password);
 
-        String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-
-        try (Connection con = DBConfig.getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
-            ps.setString(1, username);
-            ps.setString(2, password);
-            ps.executeUpdate();
-            return true;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            return rs.getString("role");
         }
-    }
-
-    // GET PASSWORD FOR LOGIN
-    public String getPasswordByUsername(String username) {
-
-        String sql = "SELECT password FROM users WHERE username = ?";
-        String password = null;
-
-        try (Connection con = DBConfig.getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
-            ps.setString(1, username);
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-                password = rs.getString("password");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return password;
+        return null;
     }
 }
