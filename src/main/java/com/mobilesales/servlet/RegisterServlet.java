eb96e5768b8b637bd3e410636ebb7379cb20eb83
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-07 14:18:58 +0530

    Update RegisterServlet.java

--- src/main/java/com/mobilesales/servlet/RegisterServlet.java
+++ src/main/java/com/mobilesales/servlet/RegisterServlet.java
@@ -1,33 +1,27 @@
 package com.mobilesales.servlet;
 
 import com.mobilesales.dao.UserDAO;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
-    private UserDAO userDAO;
-
-    public void init() {
-        userDAO = new UserDAO();
-    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String email = request.getParameter("email");
 
-        boolean success = userDAO.registerUser(username, password, email);
+        UserDAO dao = new UserDAO();
+
+        boolean success = dao.registerUser(username, password, email);
 
         if (success) {
-            response.getWriter().println("User registered successfully!");
+            response.sendRedirect("index.jsp?registerSuccess=true");
         } else {
-            response.getWriter().println("Error: User registration failed.");
+            response.sendRedirect("index.jsp?registerError=true");
         }
     }
 }
