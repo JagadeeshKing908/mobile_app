1fb89361a1ba51b38be4b76f2938c355ab020a7d
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-07 18:34:35 +0530

    Update LoginServlet.java

--- src/main/java/com/mobilesales/servlet/LoginServlet.java
+++ src/main/java/com/mobilesales/servlet/LoginServlet.java
@@ -1,43 +1,21 @@
-package com.mobilesales.servlet;
-
-import com.mobilesales.dao.UserDAO;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws IOException {
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+        String u = req.getParameter("username");
+        String p = req.getParameter("password");
 
         try {
             UserDAO dao = new UserDAO();
-            boolean isValidUser = dao.login(username, password);
-
-            if (isValidUser) {
-                HttpSession session = request.getSession();
-                session.setAttribute("username", username);
-
-                // After login show mobiles
-                response.sendRedirect("index.jsp");
+            if (dao.login(u, p)) {
+                req.getSession().setAttribute("username", u);
+                res.sendRedirect("index.jsp");
             } else {
-                // Invalid credentials
-                response.sendRedirect("login.jsp?error=invalid");
+                res.sendRedirect("index.jsp?loginError=true");
             }
-
         } catch (Exception e) {
-            e.printStackTrace();
-            response.sendRedirect("login.jsp?error=db");
+            res.sendRedirect("index.jsp?error=db");
         }
     }
 }
