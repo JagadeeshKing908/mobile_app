fed8ef485280e78f2425bf16285cc35f37b01595
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-07 19:56:32 +0530

    Update DBConfig.java

--- src/main/java/com/mobilesales/config/DBConfig.java
+++ src/main/java/com/mobilesales/config/DBConfig.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConfig {
 
-    private static final String URL = "jdbc:mysql://172.31.13.211:3306/mobilestore";
+    private static final String URL = "jdbc:mysql://172.31.13.98:3306/mobilestore";
     private static final String USER = "mobileuser";
     private static final String PASSWORD = "Mobile@123";
 
