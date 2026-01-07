6af9ab1e4f46a7c64c05fe406566d45c266f5cfd
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-07 17:02:02 +0530

    Update LoginServlet.java

--- src/main/java/com/mobilesales/servlet/LoginServlet.java
+++ src/main/java/com/mobilesales/servlet/LoginServlet.java
@@ -1,26 +1,43 @@
 package com.mobilesales.servlet;
 
 import com.mobilesales.dao.UserDAO;
-import javax.servlet.*;
-import javax.servlet.http.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        UserDAO dao = new UserDAO();
+        try {
+            UserDAO dao = new UserDAO();
+            boolean isValidUser = dao.login(username, password);
 
-        if (dao.login(username, password)) {
-            HttpSession session = request.getSession();
-            session.setAttribute("username", username);
-            response.sendRedirect("index.jsp?loginSuccess=true");
-        } else {
-            response.sendRedirect("index.jsp?loginError=true");
+            if (isValidUser) {
+                HttpSession session = request.getSession();
+                session.setAttribute("username", username);
+
+                // After login show mobiles
+                response.sendRedirect("index.jsp");
+            } else {
+                // Invalid credentials
+                response.sendRedirect("login.jsp?error=invalid");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendRedirect("login.jsp?error=db");
         }
     }
 }
