05e5d932a3a5f6e3fae227ed481964fdef44b796
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-07 14:04:05 +0530

    Update UserDAO.java

--- src/main/java/com/mobilesales/dao/UserDAO.java
+++ src/main/java/com/mobilesales/dao/UserDAO.java
@@ -1,27 +1,57 @@
-package com.mobilesales.servlet;
+package com.mobilesales.dao;
 
-import com.mobilesales.dao.UserDAO;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class RegisterServlet extends HttpServlet {
+public class UserDAO {
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    private String jdbcURL = "jdbc:mysql://172.31.5.224:3306/mobilesalesdb";
+    private String jdbcUsername = "mobileuser";
+    private String jdbcPassword = "Mobile@123";
 
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        String email = request.getParameter("email");
+    private static final String INSERT_USER_SQL = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+    private static final String SELECT_USER_BY_USERNAME = "SELECT * FROM users WHERE username = ?";
 
-        UserDAO dao = new UserDAO();
+    public UserDAO() {}
 
-        boolean success = dao.registerUser(username, password, email);
-
-        if (success) {
-            response.sendRedirect("index.jsp?registerSuccess=true");
-        } else {
-            response.sendRedirect("index.jsp?registerError=true");
+    protected Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
+        return connection;
+    }
+
+    public boolean registerUser(String username, String password, String email) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER_SQL)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, email);
+            return preparedStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean validateUser(String username, String password) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_USERNAME)) {
+            preparedStatement.setString(1, username);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                return rs.getString("password").equals(password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
