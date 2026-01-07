da55b5e5ff5f32cefc48715305268fcefc80bcc8
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-07 16:03:21 +0530

    Update DBConfig.java

--- src/main/java/com/mobilesales/config/DBConfig.java
+++ src/main/java/com/mobilesales/config/DBConfig.java
@@ -1,14 +1,12 @@
 package com.mobilesales.config;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBConfig {
 
     private static final String URL = "jdbc:mysql://172.31.5.224:3306/mobilestore";
     private static final String USER = "mobileuser";
-    private static final String PASSWORD = "Mobile@123"; // replace with your MySQL password
+    private static final String PASSWORD = "Mobile@123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
