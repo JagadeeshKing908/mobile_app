1ace6fb8e2d7dedbc19706c64ba536877f06a74a
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-07 16:03:53 +0530

    Update LogoutServlet.java

--- src/main/java/com/mobilesales/servlet/LogoutServlet.java
+++ src/main/java/com/mobilesales/servlet/LogoutServlet.java
@@ -5,12 +5,11 @@ import javax.servlet.http.*;
 import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         HttpSession session = request.getSession(false);
-        if (session != null) {
+        if(session != null) {
             session.invalidate();
         }
         response.sendRedirect("index.jsp");
