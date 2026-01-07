0fd0988b012088362003ef3929f5d5cb17b83500
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-07 13:21:23 +0530

    Update UserDAO.java

--- src/main/java/com/mobilesales/dao/UserDAO.java
+++ src/main/java/com/mobilesales/dao/UserDAO.java
@@ -1,35 +1,26 @@
 package com.mobilesales.dao;
 
 import com.mobilesales.config.DBConfig;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class UserDAO {
 
     // REGISTER USER
-    public void registerUser(String username, String password, String email) {
+    public void registerUser(String username, String password, String email) throws SQLException {
         String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
-
         try (Connection con = DBConfig.getConnection();
              PreparedStatement ps = con.prepareStatement(sql)) {
 
             ps.setString(1, username);
-            ps.setString(2, password); // plain password for now
+            ps.setString(2, password); // TODO: Hash the password for production
             ps.setString(3, email);
             ps.executeUpdate();
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
-    // LOGIN USER ✅ (THIS WAS MISSING)
-    public boolean login(String username, String password) {
-
+    // LOGIN USER
+    public boolean login(String username, String password) throws SQLException {
         String sql = "SELECT password FROM users WHERE username = ?";
-
         try (Connection con = DBConfig.getConnection();
              PreparedStatement ps = con.prepareStatement(sql)) {
 
@@ -40,11 +31,7 @@ public class UserDAO {
                 String dbPassword = rs.getString("password");
                 return dbPassword.equals(password);
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-
         return false;
     }
 }
