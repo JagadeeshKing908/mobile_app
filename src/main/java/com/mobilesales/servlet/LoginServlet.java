25399f68d4770a73cdf78a1fde9c62402390c598
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-07 14:17:50 +0530

    Update LoginServlet.java

--- src/main/java/com/mobilesales/servlet/LoginServlet.java
+++ src/main/java/com/mobilesales/servlet/LoginServlet.java
@@ -1,32 +1,30 @@
 package com.mobilesales.servlet;
 
 import com.mobilesales.dao.UserDAO;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-    private UserDAO userDAO;
-
-    public void init() {
-        userDAO = new UserDAO();
-    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        boolean valid = userDAO.validateUser(username, password);
+        UserDAO dao = new UserDAO();
 
-        if (valid) {
-            response.getWriter().println("Login successful!");
+        if (dao.login(username, password)) {
+
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+
+            response.sendRedirect("index.jsp");
+
         } else {
-            response.getWriter().println("Invalid username or password.");
+            response.sendRedirect("login.jsp?error=invalid");
         }
     }
 }
