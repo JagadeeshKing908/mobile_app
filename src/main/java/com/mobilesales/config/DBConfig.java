367dd533f0d15eddfc3d9e470941ae25447db7d4
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-07 20:31:10 +0530

    Update DBConfig.java

--- src/main/java/com/mobilesales/config/DBConfig.java
+++ src/main/java/com/mobilesales/config/DBConfig.java
@@ -8,9 +8,14 @@ public class DBConfig {
 
     private static final String URL = "jdbc:mysql://172.31.13.98:3306/mobilestore";
     private static final String USER = "mobileuser";
-    private static final String PASSWORD = "Mobile@123";
+    private static final String PASSWORD = "yourpassword";
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Ensure driver loaded
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
