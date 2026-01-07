2c88d806f73af996d2a0a4fabae1a472630c02ca
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-07 19:25:58 +0530

    Update MobileDetailsServlet.java

--- src/main/java/com/mobilesales/servlet/MobileDetailsServlet.java
+++ src/main/java/com/mobilesales/servlet/MobileDetailsServlet.java
@@ -3,26 +3,29 @@ package com.mobilesales.servlet;
 import com.mobilesales.dao.MobileDAO;
 import com.mobilesales.model.Mobile;
 
+import javax.servlet.http.*;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
-@WebServlet("/mobile-details")
 public class MobileDetailsServlet extends HttpServlet {
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         int id = Integer.parseInt(request.getParameter("id"));
 
-        MobileDAO dao = new MobileDAO();
-        Mobile mobile = dao.getMobileById(id);
+        try {
+            MobileDAO dao = new MobileDAO();
+            Mobile mobile = dao.getMobileById(id);
 
-        request.setAttribute("mobile", mobile);
-        request.getRequestDispatcher("mobile-details.jsp").forward(request, response);
+            request.setAttribute("mobile", mobile);
+            request.getRequestDispatcher("mobile-details.jsp")
+                   .forward(request, response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ServletException("Error loading mobile details", e);
+        }
     }
 }
