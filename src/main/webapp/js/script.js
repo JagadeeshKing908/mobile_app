f741f642e4ecb5b1a13873f8c8bd47cb596e66cc
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-08 13:58:37 +0530

    Update and rename popup.js to script.js

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -10,3 +10,10 @@ function closePopup() {
     document.getElementById("loginPopup").style.display = "none";
     document.getElementById("registerPopup").style.display = "none";
 }
+
+// Close popup if user clicks anywhere outside the white box
+window.onclick = function(event) {
+    if (event.target.className === "popup-overlay") {
+        closePopup();
+    }
+}
