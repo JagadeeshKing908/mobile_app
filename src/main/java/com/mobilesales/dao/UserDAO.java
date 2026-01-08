531adf92999c033196690a7d7013419986447b17
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-08 12:01:36 +0530

    Update UserDAO.java

--- src/main/java/com/mobilesales/dao/UserDAO.java
+++ src/main/java/com/mobilesales/dao/UserDAO.java
@@ -5,39 +5,32 @@ import java.sql.*;
 
 public class UserDAO {
 
-    // REGISTER USER
-    public boolean registerUser(String username, String password, String email) {
-        String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
-
-        try (Connection con = DBConfig.getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
-            ps.setString(1, username);
+    public boolean validateUser(String email, String password) {
+        try {
+            Connection con = DBConfig.getConnection();
+            PreparedStatement ps =
+                con.prepareStatement("SELECT * FROM users WHERE email=? AND password=?");
+            ps.setString(1, email);
             ps.setString(2, password);
-            ps.setString(3, email);
 
-            ps.executeUpdate();
-            return true;
+            ResultSet rs = ps.executeQuery();
+            return rs.next();
 
         } catch (Exception e) {
             e.printStackTrace();
-            return false;
         }
+        return false;
     }
 
-    // VALIDATE LOGIN
-    public boolean validateUser(String username, String password) {
-        String sql = "SELECT password FROM users WHERE username = ?";
-
-        try (Connection con = DBConfig.getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
-            ps.setString(1, username);
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-                return rs.getString("password").equals(password);
-            }
+    public boolean registerUser(String email, String password) {
+        try {
+            Connection con = DBConfig.getConnection();
+            PreparedStatement ps =
+                con.prepareStatement("INSERT INTO users(email,password) VALUES(?,?)");
+            ps.setString(1, email);
+            ps.setString(2, password);
+            ps.executeUpdate();
+            return true;
 
         } catch (Exception e) {
             e.printStackTrace();
