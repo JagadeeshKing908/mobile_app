eeab6e49fa81f628ba753b390c71e423971bacc8
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-08 12:04:54 +0530

    Update User.java

--- src/main/java/com/mobilesales/model/User.java
+++ src/main/java/com/mobilesales/model/User.java
@@ -1,16 +1,40 @@
 package com.mobilesales.model;
 
 public class User {
-    private String username;
+
+    private int id;
     private String email;
     private String password;
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public User() {
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
