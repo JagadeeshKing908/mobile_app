ca95e39029fdefc32370e07530f58f852da2e395
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-08 13:45:40 +0530

    Update and rename cart.js to popup.js

---
+++ src/main/webapp/js/popup.js
@@ -0,0 +1,12 @@
+function openLogin() {
+    document.getElementById("loginPopup").style.display = "block";
+}
+
+function openRegister() {
+    document.getElementById("registerPopup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("loginPopup").style.display = "none";
+    document.getElementById("registerPopup").style.display = "none";
+}
