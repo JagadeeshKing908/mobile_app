e1bb630934ceb4aba739a2e01308a63660324ce4
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-08 13:26:59 +0530

    Update RegisterServlet.java

--- src/main/java/com/mobilesales/servlet/RegisterServlet.java
+++ src/main/java/com/mobilesales/servlet/RegisterServlet.java
@@ -1,22 +1,22 @@
 package com.mobilesales.servlet;
 
 import com.mobilesales.dao.UserDAO;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
-import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-@WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
-        new UserDAO().registerUser(
-            req.getParameter("email"),
-            req.getParameter("password")
-        );
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
 
-        res.sendRedirect("index.jsp?registered=true");
+        UserDAO dao = new UserDAO();
+        dao.registerUser(email, password);
+
+        res.sendRedirect("login.jsp");
     }
 }
