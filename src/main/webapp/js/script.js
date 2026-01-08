3375d073dfcdd9ac2a960a707e0ed8d635edccb9
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-08 14:34:26 +0530

    Update script.js

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -2,6 +2,11 @@ function openLogin() {
     document.getElementById("loginPopup").style.display = "flex";
 }
 
+function openRegister() {
+    document.getElementById("registerPopup").style.display = "flex";
+}
+
 function closePopup() {
     document.getElementById("loginPopup").style.display = "none";
+    document.getElementById("registerPopup").style.display = "none";
 }
