bc40605a76cc1b1a071a30cbf40d24a144348593
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-08 13:18:15 +0530

    Update LoginServlet.java

--- src/main/java/com/mobilesales/servlet/LoginServlet.java
+++ src/main/java/com/mobilesales/servlet/LoginServlet.java
@@ -1,11 +1,12 @@
 package com.mobilesales.servlet;
 
 import com.mobilesales.dao.UserDAO;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
@@ -14,14 +15,11 @@ public class LoginServlet extends HttpServlet {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
 
-        UserDAO dao = new UserDAO();
-
-        if (dao.validateUser(email, password)) {
-            HttpSession session = req.getSession();
-            session.setAttribute("user", email);
+        if (new UserDAO().validateUser(email, password)) {
+            req.getSession().setAttribute("user", email);
             res.sendRedirect("index.jsp");
         } else {
-            res.sendRedirect("login.jsp?error=true");
+            res.sendRedirect("index.jsp?error=true");
         }
     }
 }
