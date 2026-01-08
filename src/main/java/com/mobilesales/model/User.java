4a80d12e1822b11e1a0eafcff5be3c2142590b04
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-01-08 13:26:12 +0530

    Update User.java

--- src/main/java/com/mobilesales/model/User.java
+++ src/main/java/com/mobilesales/model/User.java
@@ -6,8 +6,7 @@ public class User {
     private String email;
     private String password;
 
-    public User() {
-    }
+    public User() {}
 
     public User(String email, String password) {
         this.email = email;
